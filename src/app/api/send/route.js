df283b2f0b451612b1c5eb22e1af4c6956000b4b
clark <arcaneade@gmail.com>
2024-05-30 13:59:21 +1000

    fixed

--- src/app/api/send/route.js
+++
@@ -1,7 +0,0 @@
-import { EmailTemplate } from "@/components";
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-export default async (req,res)=>({
-    
-})
